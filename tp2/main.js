31c9c1d8df496ea350bcdf598116e9d0b6749ecb
zeta <angeloportelli05@gmail.com>
2024-01-28 19:39:07 +0100

    Exercice 7

--- tp2/main.js
+++ tp2/main.js
@@ -1,14 +1,19 @@
-import {Member} from "./Member.js"
-import {Team} from "./Team.js"
+// import {Member} from "./Member.js"
+// import {Team} from "./Team.js"
+import {Commune} from "./Commune.js"
+import {CommuneDetails} from "./CommuneDetails.js"
 
-let casali = new Member("Casali", "Alain", "MCF")
-let salou = new Member("Salou", "Alexandra", "PR")
+// let casali = new Member("Casali", "Alain", "MCF")
+// let salou = new Member("Salou", "Alexandra", "PR")
+//
+// let team = new Team()
+//
+// team.addMember(casali)
+// team.addMember(salou)
+//
+// for(let i of team.members) {
+//     console.log(i.toString())
+// }
 
-let team = new Team()
-
-team.addMember(casali)
-team.addMember(salou)
-
-for(let i of team.members) {
-    console.log(i.toString())
-}
+new Commune("123", "456", "789", "nom", "region", "superficie").afficheCommune()
+new CommuneDetails("123", "456", "789", "nom", "region", "superficie", "codeCommune", "codeCanton", "codeArrondissement", "codeRegion").afficheCommuneDetails()
