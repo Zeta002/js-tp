1966c396508e14e8b6677b8cb89fa442f5282719
Zeta002 <angeloportelli05@gmail.com>
2024-01-29 12:17:06 +0100

    Taquin nouvelle version, à terminer

--- tp2/slide-puzzle.js
+++ tp2/slide-puzzle.js
@@ -1,13 +1,20 @@
 let game = document.getElementById('game')
 let tiles = game.children
+let lastTile = document.getElementById('16')
 
 let moveCounterElement = document.getElementById('move-counter')
 
-let moveCounter = 0
-
 for(let i of tiles) {
     i.addEventListener('click', (e) => {
-        game.appendChild(e.target)
+        let targetHTML = e.target.innerHTML
+        let targetId = e.target.id
+        let lastTileId = lastTile.id // Sauvegarder l'identifiant de lastTile avant de le changer
+
+        e.target.innerHTML = lastTile.innerHTML
+        lastTile.id = "temporary"
+        e.target.id = lastTileId // Utiliser l'identifiant sauvegardé de lastTile
+        lastTile.id = targetId
+        lastTile.innerHTML = targetHTML
         moveCounterElement.innerHTML++
     })
 }
